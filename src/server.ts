460d3cf1269f6ff682707142b4e1a1d3a5a23a34
sadiabinta <sadiaa.kabir@gmail.com>
2025-06-22 18:22:53 +0600

    .env

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
@@ -7,7 +9,9 @@ const PORT=5000;
 
 async function main(){
     try{
-        await mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0.gwkhwjr.mongodb.net/Library-Management-System?retryWrites=true&w=majority&appName=Cluster0')
+        const uri = process.env.MONGODB_URI!;
+        await mongoose.connect(uri);
+        //await mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0.gwkhwjr.mongodb.net/Library-Management-System?retryWrites=true&w=majority&appName=Cluster0')
         server=app.listen(PORT,()=>{
             console.log(`App is listening to PORT ${PORT}`);
         })
