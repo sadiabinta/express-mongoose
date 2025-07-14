708219f4643d1a670f63127a6bcbe3de2f0483a5
sadiabinta <sadiaa.kabir@gmail.com>
2025-07-14 12:40:35 +0600

    jfoj

--- src/app.ts
+++ src/app.ts
@@ -30,8 +30,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Your routes (check these files for route param errors)
-app.use("/books", booksRoutes);
-app.use("/borrow", borrowRoutes);
+app.use("/api/books", booksRoutes);
+app.use("/api/borrow", borrowRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Welcome to the Library Management System`);
